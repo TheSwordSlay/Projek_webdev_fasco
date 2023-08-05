00d575382f3685ed9bb36790fda33af2fdc0e6c3
TheSwordSlay <107758410+TheSwordSlay@users.noreply.github.com>
2023-08-05 15:51:56 +0700

    resep v2

--- resources/views/resep.blade.php
+++ resources/views/resep.blade.php
@@ -41,35 +41,46 @@
     </div>
     <!-- akhir navbar -->
 
-    <div class="text-center text-3xl font-medium mt-8">
-        Resep
-    </div>
-
-    <div class="my-12 mx-12 flex justify-center">
-        <img src="https://2.bp.blogspot.com/-VckZ2bd4oH4/WcTUzSJiWBI/AAAAAAAAApY/7L7l2ai44yYZlghoAtrRtTRRM85e8uR7ACEwYBhgL/s1600/Kuliner%2BPedas1.jpg" class="max-h-[550px]"></img>
-    </div>
-
-    <div class="grid grid-cols-2 gap-4 ml-5">
-        <div class="text-3xl font-normal bg-slate-100">Bahan:
-        <div class="text-base"> 
-            <ul>
-                <li>2 teaspoons aniseed</li>
-                <li>4 garlic cloves, finely chopped</li>
-                <li>¼ cup fresh lime juice, plus wedges for serving</li>
-                <li>¼ cup reduced-sodium soy sauce</li>
-                <li>2 tablespoons honey</li>
-                <li>8 chicken thighs (about 4 lb.)</li>
-            </ul>
-        </div>
-        </div>
-
-        <div class="text-3xl font-normal">Cara:</div>
-        <div class="text-base"> 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nulla elit, eleifend ut finibus ac, mollis id eros. Phasellus felis mauris, auctor imperdiet orci non, finibus vehicula libero. Aliquam id augue at ex consectetur viverra. Duis sed tincidunt tortor. Vivamus finibus eget mauris tempor aliquam. Suspendisse porta vestibulum lorem et tincidunt. Suspendisse potenti. Vestibulum non volutpat magna. Maecenas semper, ex eget commodo posuere, odio lorem auctor urna, sit amet rhoncus erat ex eget est.
-
-            Quisque auctor ipsum felis, nec venenatis quam ullamcorper eu. Donec convallis orci vitae ultricies tincidunt. Sed at nisi placerat, hendrerit est finibus, gravida nisi. Quisque cursus nulla pulvinar purus consequat, et tempus elit euismod. Praesent tempor odio mi, sit amet maximus turpis convallis eu. Nam id purus sapien. Sed eleifend libero mollis volutpat vulputate. Sed rhoncus dapibus odio, et tempus quam molestie nec. Cras eu leo diam. Integer vitae aliquam arcu, at finibus ante. Vestibulum sit amet tortor sapien. Curabitur condimentum quam quis metus lacinia euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer convallis bibendum felis vitae iaculis.
-        </div>
+    <div class="container px-4">
+      <div class="text-center text-3xl font-medium mt-8">
+          Resep
       </div>
+  
+      <div class="my-12 mx-12 flex justify-center">
+          <img src="https://2.bp.blogspot.com/-VckZ2bd4oH4/WcTUzSJiWBI/AAAAAAAAApY/7L7l2ai44yYZlghoAtrRtTRRM85e8uR7ACEwYBhgL/s1600/Kuliner%2BPedas1.jpg" class="max-h-[550px]"></img>
+      </div>
+  
+      <h1 class="font-bold">
+        Deskripsi
+      </h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias pariatur molestias ab unde nemo non magni veritatis assumenda enim praesentium?</p>
+  
+      <div class="grid grid-cols-4 gap-4 ml-5 my-10">
+          <div class="text-3xl font-normal bg-slate-100 px-8">Bahan:
+            <div class="text-base"> 
+                <ul class="list-disc block bg-slate-100">
+                    <li>2 teaspoons aniseed</li>
+                    <li>4 garlic cloves, finely chopped</li>
+                    <li>¼ cup fresh lime juice, plus wedges for serving</li>
+                    <li>¼ cup reduced-sodium soy sauce</li>
+                    <li>2 tablespoons honey</li>
+                    <li>8 chicken thighs (about 4 lb.)</li>
+                </ul>
+            </div>
+          </div>
+  
+          <div class="text-3xl font-normal col-span-3 px-8">
+            Cara:
+            <div class="text-base"> 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nulla elit, eleifend ut finibus ac, mollis id eros. Phasellus felis mauris, auctor imperdiet orci non, finibus vehicula libero. Aliquam id augue at ex consectetur viverra. Duis sed tincidunt tortor. Vivamus finibus eget mauris tempor aliquam. Suspendisse porta vestibulum lorem et tincidunt. Suspendisse potenti. Vestibulum non volutpat magna. Maecenas semper, ex eget commodo posuere, odio lorem auctor urna, sit amet rhoncus erat ex eget est.
+  
+            Quisque auctor ipsum felis, nec venenatis quam ullamcorper eu. Donec convallis orci vitae ultricies tincidunt. Sed at nisi placerat, hendrerit est finibus, gravida nisi. Quisque cursus nulla pulvinar purus consequat, et tempus elit euismod. Praesent tempor odio mi, sit amet maximus turpis convallis eu. Nam id purus sapien. Sed eleifend libero mollis volutpat vulputate. Sed rhoncus dapibus odio, et tempus quam molestie nec. Cras eu leo diam. Integer vitae aliquam arcu, at finibus ante. Vestibulum sit amet tortor sapien. Curabitur condimentum quam quis metus lacinia euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer convallis bibendum felis vitae iaculis.
+            </div>
+  
+          </div>
+        </div>
+
+    </div>
 
 </body>
 </html>
